7116f471ff5c40b5e26090f9d289e735cbc0dfd3
muhazAhmed <muhazvla313@gmail.com>
2025-08-23 15:50:07 +0530

    chore: added page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Usage from "./pages/Usage";
 
 function App() {
-
   return (
-    <></>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/usage" element={<Usage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
