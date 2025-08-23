9b82589183caf61a38817d7f44f6195685bb3d5b
muhazAhmed <muhazvla313@gmail.com>
2025-08-23 23:39:52 +0530

    fix: removed docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Usage from "./pages/Usage";
-
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/usage" element={<Usage />} />
-      </Routes>
-    </Router>
-  );
+  return <></>;
 }
 
 export default App;
